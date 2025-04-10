29c7ee88a45d4de723f228f2082933f9cd9436c8
Agent4growth <agent4grow@gmail.com>
2025-04-10 11:42:17 +0200

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Agent4GrowthLanding from './Agent4GrowthLanding'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Agent4GrowthLanding />
+  </React.StrictMode>,
+)
